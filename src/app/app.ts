e4391a9e53d7f1555723f6d6061be241f0769163
AnsariAffan007 <ansariaffan014@gmail.com>
2025-10-15 12:31:19 +0530

    navbar, font-awesome, tailwind palette

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome"
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [RouterOutlet]
+  imports: [RouterOutlet, FontAwesomeModule]
 })
 export class App {
   protected readonly title = signal('portfolio_pro-angular');
+  faLinkedin = faLinkedin
+  faGithub = faGithub
 }
