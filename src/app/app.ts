001a285f869308ff3c8abcf5a5dd225433ecc514
AnsariAffan007 <ansariaffan014@gmail.com>
2025-10-16 11:45:36 +0530

    intro section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,9 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome"
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
+import { config } from '@fortawesome/fontawesome-svg-core';
+// Tell Font Awesome to skip adding the CSS automatically since it's being imported in styles.scss
+config.autoAddCss = false; 
 
 @Component({
   selector: 'app-root',
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { faAddressBook } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
+  faAddressBook = faAddressBook
 
+  descHovering: boolean = false;
+
+  descMouseEnter() {
+    this.descHovering = true
+  }
+  descMouseLeave() {
+    this.descHovering = false;
+  }
 }
