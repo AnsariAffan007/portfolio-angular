045dae1685e89a98b7cc3009138aa654cce89c54
AnsariAffan007 <ansariaffan014@gmail.com>
2025-10-27 11:20:50 +0530

    navbar menu icon, responsiveness

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome"
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 import { config } from '@fortawesome/fontawesome-svg-core';
 // Tell Font Awesome to skip adding the CSS automatically since it's being imported in styles.scss
-config.autoAddCss = false; 
+config.autoAddCss = false;
 
 @Component({
   selector: 'app-root',
@@ -16,4 +17,5 @@ export class App {
   protected readonly title = signal('portfolio_pro-angular');
   faLinkedin = faLinkedin
   faGithub = faGithub
+  faBars = faBars
 }
