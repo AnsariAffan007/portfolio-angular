7e748cf27a551a1936b6a45262f794b2ac96e67b
AnsariAffan007 <ansariaffan014@gmail.com>
2025-10-27 12:07:07 +0530

    nav dropdown for smaller screens

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,7 @@ import { FontAwesomeModule } from "@fortawesome/angular-fontawesome"
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
 import { faBars } from "@fortawesome/free-solid-svg-icons"
 import { config } from '@fortawesome/fontawesome-svg-core';
+import { NgClass } from "@angular/common";
 // Tell Font Awesome to skip adding the CSS automatically since it's being imported in styles.scss
 config.autoAddCss = false;
 
@@ -11,11 +12,17 @@ config.autoAddCss = false;
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [RouterOutlet, FontAwesomeModule, RouterLinkWithHref]
+  imports: [RouterOutlet, FontAwesomeModule, RouterLinkWithHref, NgClass]
 })
 export class App {
   protected readonly title = signal('portfolio_pro-angular');
+
   faLinkedin = faLinkedin
   faGithub = faGithub
   faBars = faBars
+
+  menuOpen: boolean = false;
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen
+  }
 }
