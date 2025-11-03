4baf72a165203f87d1426ac3a4d5a911a6d93309
AnsariAffan007 <ansariaffan014@gmail.com>
2025-11-03 11:57:21 +0530

    contact me section field styling logic

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircleCheck, faLightbulb } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
 export class Contact {
+  faLightBulb = faLightbulb
+  faCircleCheck = faCircleCheck
 
+  emailFocused = false;
+  messageFocused = false;
+  toggleFieldFocus(field: "email" | "message", isFocused: boolean) {
+    if (field === "email") this.emailFocused = isFocused;
+    else this.messageFocused = isFocused
+  }
+
+  resizeTextArea(event: Event): void {
+    const textarea = event.target as HTMLTextAreaElement;
+    textarea.style.height = textarea.style.minHeight = '100%';
+    textarea.style.minHeight = `${Math.min(textarea.scrollHeight + 2, parseInt(textarea.style.maxHeight))}px`;
+    textarea.style.height = `${textarea.scrollHeight + 2}px`;
+  }
 }
