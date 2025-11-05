342544680cc309ad5a8958e6ee493c61a9c05cc0
AnsariAffan007 <ansariaffan014@gmail.com>
2025-11-05 11:48:46 +0530

    work -- populate links

--- src/app/pages/work/work.ts
+++ src/app/pages/work/work.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import data from "./projects.json"
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+import { faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-work',
-  imports: [],
+  imports: [FaIconComponent],
   templateUrl: './work.html',
   styleUrl: './work.scss'
 })
 export class Work {
   projects = Object.values(data)
+
+  faArrowUpRightFromSquare = faArrowUpRightFromSquare
+  faGithub = faGithub
+  faYoutube = faYoutube
 }
